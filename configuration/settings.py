670b6252cd7c02d9abe33041e268978fe8bb800c
Nelson <neldecas12@gmail.com>
2023-05-06 15:51:54 -0500

    :

--- configuration/settings.py
+++ configuration/settings.py
@@ -257,8 +257,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "egresados398@gmail.com"
-EMAIL_HOST_PASSWORD = "gcoowxkdvzyrupqy"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 
 
 # Internationalization
