c9d95e1063670c024a596030cbe581ff50d72a1f
Nelson <neldecas12@gmail.com>
2023-05-21 17:01:11 -0500

    bulk delete

--- apps/classified_advertisements/api/views/capacitaciones/urls.py
+++ apps/classified_advertisements/api/views/capacitaciones/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("",CapacitacionesView.as_view()),
     path("create/",SaveCapacitacionesView.as_view()),
     path("update/<int:pk>/",UpdateCapacitacionesView.as_view()),
+    path("delete/",DeleteCapacitacionesView.as_view()),
     path("delete/<int:pk>/",DeleteCapacitacionesView.as_view())
 ]
