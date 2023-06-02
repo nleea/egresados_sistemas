75ca3fb361d2d83830966555716c405843a1fa15
nleea <neldecas12@gmail.com>
2023-06-02 11:06:38 -0500

    :

--- configuration/wsgi.py
+++ configuration/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configuration.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.MEDIA_ROOT)
+# application.add_files("/path/to/more/static/files", prefix="more-files/")
