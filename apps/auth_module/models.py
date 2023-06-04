fee8377407096caf1153056a0eecda3cf1d323bd
nleea <neldecas12@gmail.com>
2023-06-04 17:16:38 -0500

    upload changes in the models

--- apps/auth_module/models.py
+++ apps/auth_module/models.py
@@ -80,11 +80,11 @@ class Persons(BaseModel):
     surname = models.CharField(
         max_length=150, unique=True, blank=True, null=True)
     identification = models.CharField(
-        max_length=255, unique=True, blank=True, null=True)
+        max_length=40, unique=True, blank=True, null=True)
     address = models.CharField(max_length=50, blank=True, null=True)
     nationality = models.CharField(max_length=100, blank=True, null=True)
     date_of_birth = models.CharField(max_length=30, blank=True, null=True)
-    phone = models.CharField(max_length=20, blank=True, null=True)
+    phone = models.CharField(max_length=11, blank=True, null=True)
     status = models.BooleanField(default=True)
     document_type = models.ForeignKey(
         Document_types, on_delete=models.SET_NULL, blank=True, null=True)
