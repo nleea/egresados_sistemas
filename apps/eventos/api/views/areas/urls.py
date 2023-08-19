2fca985c1bbb438f3b315e79f641c994481212aa
nleea <neldecas12@gmail.com>
2023-08-19 16:09:28 -0500

    areas eventos post test

--- apps/eventos/api/views/areas/urls.py
+++ apps/eventos/api/views/areas/urls.py
@@ -10,7 +10,7 @@ app_name = "areas"
 
 urlpatterns = [
     path("", EventosAreaView.as_view(), name="areas-list"),
-    path("create/", SaveEventosAreaView.as_view()),
+    path("create/", SaveEventosAreaView.as_view(), name="areas-create"),
     path("update/<int:pk>/", UpdateEventosAreaView.as_view()),
     path("delete/<int:pk>/", DeleteEventosAreaView.as_view()),
     path("delete/", DeleteEventosAreaView.as_view()),
--- apps/eventos/test/test_eventos_area.py
+++ apps/eventos/test/test_eventos_area.py
@@ -25,7 +25,7 @@ def test_get_areas_should_ok(client: Client, user_token):
     indirect=True,
     ids=["areas"],
 )
-def test_get_areas_list_shouls_ok(client: Client, user_token, areas,clear_cache):
+def test_get_areas_list_shouls_ok(client: Client, user_token, areas, clear_cache):
     list_areas = set(map(lambda x: x.name, areas))
 
     response = client.get(
@@ -37,3 +37,42 @@ def test_get_areas_list_shouls_ok(client: Client, user_token, areas,clear_cache)
     assert len(response_content["data"]) == len(list_areas)
     list_response_areas = set(map(lambda x: x.get("name"), response_content["data"]))
     assert list_response_areas == list_areas
+
+
+# ------ test POST ------- #
+
+
+def test_post_create_should_ok(client: Client, user_token):
+    response = client.post(
+        reverse("eventos:areas:areas-create"),
+        data={"name": "Area1"},
+        HTTP_AUTHORIZATION=f"Bearer {user_token}",
+    )
+    assert response.status_code == 200
+    response_content = response.json()
+    assert response_content["data"] == '"Success"'
+    assert response_content["ok"] == True
+
+
+def test_post_create_should_faild(client: Client, user_token):
+    response = client.post(
+        reverse("eventos:areas:areas-create"),
+        HTTP_AUTHORIZATION=f"Bearer {user_token}",
+    )
+    assert response.status_code == 400
+    response_content = response.json()
+    assert response_content["errors"] == {"name": ["This field is required."]}
+    assert response_content["ok"] == False
+
+
+@pytest.mark.parametrize("area", ["Area1", "Area2", "Area3"])
+def test_post_create_parameters_should_ok(client, user_token, area):
+    response = client.post(
+        reverse("eventos:areas:areas-create"),
+        data={"name": area},
+        HTTP_AUTHORIZATION=f"Bearer {user_token}",
+    )
+    assert response.status_code == 200
+    response_content = response.json()
+    assert response_content["data"] == '"Success"'
+    assert response_content["ok"] == True
