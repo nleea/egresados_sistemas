1556eedf68fd776e07316057e721829f1d900bc6
nleea <neldecas12@gmail.com>
2023-08-18 21:33:56 -0500

    stop redis server

--- configuration/settings.py
+++ configuration/settings.py
@@ -223,16 +223,16 @@ SIMPLE_JWT = {
 
 CACHE_TTL = 60 * 15
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        },
-        "KEY_PREFIX": "cache",
-    }
-}
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         },
+#         "KEY_PREFIX": "cache",
+#     }
+# }
 
 
 CELERY_BROKER_URL = "redis://127.0.0.1:6379"
