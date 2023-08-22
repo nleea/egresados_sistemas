574fd9fb0d3c57be9e1063679d9d2ac1b7727f67
nleea <neldecas12@gmail.com>
2023-08-22 14:06:30 -0500

    :

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,6 +1,6 @@
 # scp -i /tmp/prueba.pem -r /var/jenkins_home/workspace/pipelines-test/ ubuntu@ec2-18-216-190-103.us-east-2.compute.amazonaws.com:/home/ubuntu/main/
-scp -i /tmp/prueba.pem ./scripts/deploy/publish ubuntu@ec2-18-216-190-103.us-east-2.compute.amazonaws.com:/tmp/publish
-ssh -i /tmp/prueba.pem  ubuntu@ec2-18-216-190-103.us-east-2.compute.amazonaws.com /tmp/publish
+scp -i /tmp/prueba.pem ./scripts/deploy/publish ubuntu@44.203.185.252:/tmp/publish
+ssh -i /tmp/prueba.pem  ubuntu@44.203.185.252 /tmp/publish
 
 echo "before deploy"
 echo "DEPLOY"
