594666589ee11e45cbe28630d7e1f36e3bebc77a
nleea <neldecas12@gmail.com>
2023-08-29 18:07:23 -0500

    added admin site register for sub-categori

--- apps/classified_advertisements/admin.py
+++ apps/classified_advertisements/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import *
+from .models import Categoria,SubCategoria
 
 
 # Register your models here.
 admin.site.register(Categoria)
+admin.site.register(SubCategoria)
