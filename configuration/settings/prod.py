f1bc007a4a0df788d9e3bc7fa5351a9325b2216e
nleea <neldecas12@gmail.com>
2023-09-02 11:40:33 -0500

    add ssh in workflow

--- configuration/settings/prod.py
+++ configuration/settings/prod.py
@@ -17,8 +17,6 @@ from configuration.db import DBS
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-print("PRO")
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
