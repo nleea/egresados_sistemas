0d7865ad20099ecd862647ef92f69dd7cc3d061b
Nelson de castro <neldecas12@gmail.com>
2023-09-07 15:46:49 -0500

    fixed router path

--- apps/reportes/api/view/encuestas/urls.py
+++ apps/reportes/api/view/encuestas/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,re_path
 from .view import ReportesUserFaculta, ReportesUserFacultaWith
 
 urlpatterns = [
-    path("faculta/", ReportesUserFaculta.as_view()),
+    path("facultad/", ReportesUserFaculta.as_view()),
     path("respuesta/<slug:filter>/", ReportesUserFacultaWith.as_view()),
     path("respuesta/<slug:filter>/<int:facultad>/", ReportesUserFacultaWith.as_view()),
 ]
