e61e13253780758e7a56580b76c0f3da6de8118d
Nelson de castro <neldecas12@gmail.com>
2023-09-07 15:40:10 -0500

    fixed router in reports

--- apps/reportes/api/view/encuestas/urls.py
+++ apps/reportes/api/view/encuestas/urls.py
@@ -3,6 +3,6 @@ from .view import ReportesUserFaculta, ReportesUserFacultaWith
 
 urlpatterns = [
     path("faculta/", ReportesUserFaculta.as_view()),
-    path("<slug:filter>/", ReportesUserFacultaWith.as_view()),
-    path("<slug:filter>/<int:facultad>/", ReportesUserFacultaWith.as_view()),
+    path("respuesta/<slug:filter>/", ReportesUserFacultaWith.as_view()),
+    path("respuesta/<slug:filter>/<int:facultad>/", ReportesUserFacultaWith.as_view()),
 ]
