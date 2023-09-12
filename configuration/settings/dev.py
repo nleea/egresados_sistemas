a074a8d54ed89d91e5101153d45ae178732c9dbb
Nelson de castro <neldecas12@gmail.com>
2023-09-12 16:27:38 -0500

    fixed settings

--- configuration/settings/dev.py
+++ configuration/settings/dev.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
-from configuration.db import DBS
+# from configuration.db import DBS
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 print("DEV")
@@ -132,14 +132,14 @@ WSGI_APPLICATION = "configuration.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     },
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+}
 
-DATABASES = DBS["mysql-docker"]
+# DATABASES = DBS["mysql-docker"]
 
 
 # Password validation
