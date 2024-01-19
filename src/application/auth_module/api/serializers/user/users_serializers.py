6a45816c5daf68ee8ada35ec64950c30a04d005c
Nelson <neldecas12@gmail.com>
2024-01-19 16:38:20 -0500

    added view update user

--- src/application/auth_module/api/serializers/user/users_serializers.py
+++ src/application/auth_module/api/serializers/user/users_serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.validators import UniqueValidator
 from ..customValidators.usersValidators import UserValidatorBefore
 from src.application.auth_module.models import Persons, Document_types, Genders
-
+from django.db.transaction import atomic, rollback
 
 User = get_user_model()
 
@@ -14,7 +14,7 @@ class PersonsSimpleSerializers(serializers.Serializer):
     name = serializers.CharField(write_only=True)
     document_type = serializers.IntegerField(write_only=True)
     surname = serializers.CharField(write_only=True)
-    identification = serializers.IntegerField(write_only=True)
+    identification = serializers.CharField(write_only=True)
     address = serializers.CharField(write_only=True)
     nationality = serializers.CharField(write_only=True)
     date_of_birth = serializers.DateField(write_only=True)
@@ -58,41 +58,54 @@ class CreateUserSerializers(serializers.Serializer):
 
     class Meta:
         model = User
-        validators = [UserValidatorBefore()]
+        # validators = [UserValidatorBefore()]
 
     def create(self, validated_data):
         validated_data["password"] = make_password(validated_data["password"])
         return User.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.username = validated_data.get("username", instance.username)
-        instance.name = validated_data.get("name", instance.name)
-        instance.surname = validated_data.get("surname", instance.surname)
-        instance.email = validated_data.get("email", instance.email)
+        try:
+            with atomic():
+                
+                instance.username = validated_data.get("username", instance.username)
+                instance.email = validated_data.get("email", instance.email)
 
-        person = Persons.objects.get(user__id=instance.pk)
-        persona = validated_data.get("persona", {})
+                person = Persons.objects.get(user__id=instance.pk)
+                persona = validated_data.get("persona", {})
 
-        person.name = persona.get("name", person.name)
-        person.surname = persona.get("surname", person.surname)
-        person.identification = persona.get("identification", person.identification)
-        person.address = persona.get("address", person.address)
-        person.nationality = persona.get("nationality", person.nationality)
-        person.date_of_birth = persona.get("date_of_birth", person.date_of_birth)
-        person.phone = persona.get("phone", person.phone)
+                person.name = persona.get("name", person.name)
+                person.surname = persona.get("surname", person.surname)
+                person.identification = persona.get(
+                    "identification", person.identification
+                )
+                person.address = persona.get("address", person.address)
+                person.nationality = persona.get("nationality", person.nationality)
+                person.date_of_birth = persona.get(
+                    "date_of_birth", person.date_of_birth
+                )
+                person.phone = persona.get("phone", person.phone)
 
-        new_document_type_id = persona.get("document_type")
-        if new_document_type_id:
-            new_document_type = Document_types.objects.get(pk=new_document_type_id)
-            person.document_type = new_document_type
+                new_document_type_id = persona.get("document_type")
+                if new_document_type_id:
+                    new_document_type = Document_types.objects.get(
+                        pk=new_document_type_id
+                    )
+                    person.document_type = new_document_type
 
-        gender_type = persona.get("gender_type")
-        if gender_type:
-            new_document_type = Genders.objects.get(pk=gender_type)
-            person.gender_type = new_document_type
+                gender_type = persona.get("gender_type")
+                if gender_type:
+                    new_document_type = Genders.objects.get(pk=gender_type)
+                    person.gender_type = new_document_type
 
-        person.save()
-        return instance
+                print("PASO")
+
+                person.save()
+                instance.save()
+                return instance
+        except Exception as e:
+            rollback()
+            raise e
 
 
 class UserSerializersSimpleRegister(serializers.ModelSerializer):
